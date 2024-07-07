dd4f3f7fb32b94a59ce6865d6f72f8ea623e3732
Fesyse <maksimlol.mail.com@mail.ru>
2024-07-07 14:38:13 +0800

    fix: theme provider

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -2,5 +2,9 @@ import type { FC, PropsWithChildren } from "react"
 import { ThemeProvider } from "./theme-provider"
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
-	return <ThemeProvider>{children}</ThemeProvider>
+	return (
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+			{children}
+		</ThemeProvider>
+	)
 }
