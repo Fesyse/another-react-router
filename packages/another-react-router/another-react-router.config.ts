29a80faf4c0b167ad7c96d15a366cf0ecba76d71
Fesyse <maksimlol.mail.com@mail.ru>
2024-07-17 16:09:21 +0800

    a

--- packages/another-react-router/another-react-router.config.ts
+++
@@ -1,9 +0,0 @@
-
-// THIS FILE SHOULD NOT BE MODIFIED
-// With love by another-react-router developers 💗
-import { type RawRoute, getRoutesComponents } from "another-react-router"
-
-const rawRoutes = [{path:"/",page:import('./src/routes/page.tsx'),layout:import('./src/routes/layout.tsx'),useOleg:true}] as const satisfies RawRoute[]
-const routes = await getRoutesComponents(rawRoutes)
-
-export { routes }
