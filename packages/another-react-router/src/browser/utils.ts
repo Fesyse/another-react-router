6f6426f85dc4b9ca7a9cd14589f758fec404fc2e
Fesyse <maksimlol.mail.com@mail.ru>
2024-07-23 15:25:23 +0800

    fix: spread routes was not working
    i hope its last fix with spread routes

--- packages/another-react-router/src/browser/utils.ts
+++ packages/another-react-router/src/browser/utils.ts
@@ -7,14 +7,31 @@ const isRouterPathMatcheWithCurrentPath = (
 	const splittedRouterPath = routerPath.split("/").filter(path => path !== "")
 	const splittedPath = path.split("/").filter(path => path !== "")
 
+	if (
+		splittedRouterPath.length !== splittedPath.length &&
+		splittedRouterPath.every(
+			path => !(path.startsWith("[...") && path.endsWith("]"))
+		)
+	)
+		return false
+
 	for (let i = 0; i < splittedRouterPath.length; i++) {
 		if (
 			splittedRouterPath[i]?.startsWith("[") &&
 			splittedRouterPath[i]?.endsWith("]")
 		) {
-			if (splittedRouterPath[i]?.startsWith("[...")) break
-			else continue
+			if (!splittedRouterPath[i]?.startsWith("[...")) continue
+
+			// making sure everything before spread route matches
+			if (
+				isRouterPathMatcheWithCurrentPath(
+					splittedRouterPath.slice(0, i).join("/"),
+					splittedPath.slice(0, i).join("/")
+				)
+			)
+				break
 		}
+
 		if (splittedRouterPath[i] !== splittedPath[i]) return false
 	}
 	return true
