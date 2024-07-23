3c5cec199edcac4f8a6ee247f84e91b9e536b062
Fesyse <maksimlol.mail.com@mail.ru>
2024-07-23 15:01:47 +0800

    fix: splittedRoutePathname was not filtering for empty strings

--- packages/another-react-router/src/browser/hooks/use-params.ts
+++ packages/another-react-router/src/browser/hooks/use-params.ts
@@ -11,14 +11,15 @@ const useParams = (): Params => {
 
 	for (const routePathname of routesPathnames) {
 		if (isRouterPathMatcheWithCurrentPath(routePathname, pathname)) {
-			const splittedRoutePathname = routePathname.split("/")
+			const splittedRoutePathname = routePathname
+				.split("/")
+				.filter(path => path !== "")
 			splittedRoutePathname.map((path, i) => {
 				if (path.startsWith("[") && path.endsWith("]")) {
 					// checking if pathg starts with "[..." if so removing 4 letters
 					// otherwise 1 because of "["
 					const isSpreadRoute = path.startsWith("[...")
 					const paramName = path.slice(isSpreadRoute ? 4 : 1, -1)
-					console.log(splittedPathname)
 					params[paramName] = isSpreadRoute
 						? splittedPathname.slice(i, splittedPathname.length)
 						: splittedPathname[i]
