61d13fb849eba0fd15b75e66eae99db3a2894fc5
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-04 13:11:05 +0800

    feat: cool spotlight effect on home page

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -6,6 +6,7 @@ import { siteConfig } from "@/config/site"
 import { Footer } from "@/components/footer"
 import { Header } from "@/components/header"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Spotlight } from "@/components/ui/spotlight"
 import { cn } from "@/lib/utils"
 import "@/styles/globals.css"
 
@@ -32,8 +33,12 @@ export default function RootLayout({ children }: PropsWithChildren) {
 				)}
 			>
 				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-					<div className="relative flex min-h-screen flex-col bg-background">
+					<div className="relative flex min-h-screen flex-col bg-background antialiased dark:bg-grid-white/[0.02]">
 						<Header />
+						<Spotlight
+							className="dark:block hidden -top-40 left-0 md:left-12 md:-top-20"
+							fill="rgba(255,255,255,0.3)"
+						/>
 						<main className="flex-1 min-h-[calc(100vh-3.5rem)] before:dark:hidden before:z-50 before:w-72 before:aspect-square before:bg-red-500/50 before:fixed before:left-2 before:top-12 before:blur-[100px] before:rounded-full after:z-50 after:dark:hidden after:w-72 after:aspect-square after:bg-red-500/50 after:fixed after:right-2 after:-bottom-2 after:blur-[100px] after:rounded-full">
 							{children}
 						</main>
--- docs/src/components/footer.tsx
+++ docs/src/components/footer.tsx
@@ -3,7 +3,7 @@ import { siteConfig } from "@/config/site"
 
 export function Footer() {
 	return (
-		<footer className="border-t py-6 px-8">
+		<footer className="relative z-50 border-t py-6 px-8">
 			<div className="container w-full flex items-center gap-2 text-sm text-foreground/50 max-sm:flex-col max-sm:gap-0.5">
 				Build with love by
 				<Link
