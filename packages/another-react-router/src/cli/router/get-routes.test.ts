711b1fc9d1d96349db0ec2f5b01f52c55a89a567
Fesyse <maksimlol.mail.com@mail.ru>
2024-08-08 16:28:14 +0800

    test: flat routes, get routes

--- packages/another-react-router/src/cli/router/get-routes.test.ts
+++ packages/another-react-router/src/cli/router/get-routes.test.ts
@@ -2,34 +2,44 @@ import { expect, test } from "vitest"
 import { getRoutes } from "./get-routes"
 import * as path from "path"
 import * as fs from "fs"
-import { RawRoute } from "@/browser"
-import { getRouteAsString } from "../commands/initialize-routes"
+import {
+  FlattenRoute,
+  getRoutesComponents,
+  PageComponent,
+  RawRoute,
+  RouteWithModules,
+} from "@/browser"
+import { flatRoutes } from "@/browser/utils"
+import { createElement } from "react"
 
 const cwd = process.cwd()
 const routesPath = "test/routes/"
 const rootDirectoryPath = path.join(cwd, routesPath)
 
-test("basic route tree", () => {
+const mockPage: string = "export default function Page() {}"
+
+test("basic route tree", async () => {
   // "./routes/"
   fs.mkdirSync(rootDirectoryPath)
-  fs.appendFileSync(path.join(rootDirectoryPath, "page.tsx"), "")
+  fs.appendFileSync(path.join(rootDirectoryPath, "page.tsx"), mockPage)
 
   const userDirectory = path.join(rootDirectoryPath, "user")
   // "./routes/user/"
   fs.mkdirSync(userDirectory)
-  fs.appendFileSync(path.join(userDirectory, "page.tsx"), "")
+  fs.appendFileSync(path.join(userDirectory, "page.tsx"), mockPage)
 
   const userIdDirectory = path.join(userDirectory, "[id]")
   // "./routes/user/[id]/"
   fs.mkdirSync(userIdDirectory)
-  fs.appendFileSync(path.join(userIdDirectory, "page.tsx"), "")
+  fs.appendFileSync(path.join(userIdDirectory, "page.tsx"), mockPage)
 
-  const routes = getRoutes({
+  const rawRoutes = getRoutes({
     routesPath,
     cwd,
   })
   fs.rmdirSync(rootDirectoryPath, { recursive: true })
-  expect(routes).toEqual<RawRoute[]>([
+
+  expect(rawRoutes).toEqual<RawRoute[]>([
     {
       path: "/",
       page: "test/routes/page.tsx",
